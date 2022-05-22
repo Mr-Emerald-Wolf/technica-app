3a6f9513632427af0b48bf10243b0804b5093be6
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2022-05-22 14:11:20 +0530

    Logo added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,13 +3,18 @@ import { Link } from "react-router-dom";
 
 
 export default function Landing() {
+    // const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    // console.log(isDarkMode);
    
     return (
         <>
             <section className="flex h-screen flex-col items-center px-6 lg:px-32 bg-orange-50 dark:bg-[#10332d] justify-center ">
                 <main id='landing' className="w-full md:w-9/12 xl:w-8/12">
-
-                    <h1 className="text-6xl md:text-6xl lg:text-9xl md:p-3 font-kalam font-bold dark:text-[#c3c3ae] text-lime-800 text-center">
+                    <div className="flex justify-center">
+                        <img className='w-1/2 -mb-6  dark:hidden' src="./assets/logo_light.png" alt="Logo" />
+                        {<img className='w-1/2 -mb-6  hidden dark:block' src="./assets/logo_dark.png" alt="Logo" />}
+                    </div>
+                    <h1 className="text-6xl md:text-6xl lg:text-9xl -mt-6 md:p-3 font-kalam font-bold dark:text-[#c3c3ae] text-lime-800 text-center">
                         MedTrack
                     </h1>
                     <p className="mb-4 text-2xl lg:text-3xl font-bold text-center text-lime-600 dark:text-[#8eca60] ">YOUR WELLBEING PARTNER</p>
