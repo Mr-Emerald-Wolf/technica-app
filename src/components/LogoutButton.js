f191394330fb871e1f95a6db29bc2d9fa4772357
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2022-05-22 15:27:02 +0530

    LOG out push

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -5,7 +5,7 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button className="flex items-center px-4 -mb-1 hover:text-lime-600  dark:border-transparent text-lime-800 dark:text-[#96b565]" onClick={() => logout({ returnTo: window.location.origin })}>
+    <button className="flex items-center px-4 -mb-1 hover:text-lime-600  dark:border-transparent text-lime-800 dark:text-[#96b565]" onClick={() => logout({ returnTo: "https://medtrack.netlify.app" })}>
       Log Out
     </button>
   );
